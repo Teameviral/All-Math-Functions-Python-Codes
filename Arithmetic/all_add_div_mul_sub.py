3ba6119dbef6c9eb0839f980c362b4fb222f0c8c
E V I R A L <74889213+Teameviral@users.noreply.github.com>
2023-01-15 15:07:12 +0530

    Create all_add_div_mul_sub.py

---
+++ Arithmetic/all_add_div_mul_sub.py
@@ -0,0 +1,16 @@
+def add_sub_mul_div(x, y):
+
+    add = x + y
+
+    sub = x - y
+
+    mul = x * y
+
+    div = x / y
+
+    return add, sub, mul, div
+
+result = add_sub_mul_div(5,2)
+
+print(result)
+
